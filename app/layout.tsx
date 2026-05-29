f5edf579d3acf19b888b6a5060e09cbdbc6f1573
rakesh <id4phn300@gmail.com>
2026-05-29 15:44:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,15 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          background: "#f3f4f6",
-        }}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
